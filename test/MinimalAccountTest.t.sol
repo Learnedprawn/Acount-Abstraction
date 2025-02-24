c8caae517469d0ad08900626994fa3f0352dad82
Learnedprawn <vineshparalkar@gmail.com>
2025-02-24 16:15:53 +0530

    script complete

---
+++ test/MinimalAccountTest.t.sol
@@ -0,0 +1,9 @@
+//SPDX-License-Identifier:MIT
+pragma solidity 0.8.24;
+
+import {Test} from "forge-std/Test.sol";
+import {MinimalAccount} from "../src/ethereum/MinimalAccount.sol";
+
+contract MinimalAccountTest is Test {
+    function setUp() public {}
+}
